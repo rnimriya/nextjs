81be7f19ba6d311ec0c83285961047ce63de510e
Rakesh Kumar <r.nimriya@gmail.com>
2024-09-27 19:39:19 +0530

    Next JS Basic things
    Next.js is a React framework for building full-stack web applications. You use React Components to build user interfaces, and Next.js for additional features and optimizations.

---
+++ src/app/blog/first/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FirstBlog() {
+  return (
+    <>
+      <h1>First Blog</h1>
+    </>
+  )
+}
+
+export default FirstBlog
---
+++ src/app/blog/second/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SecondBlog() {
+  return (
+    <>
+      <h1>Second Blog</h1>
+    </>
+  )
+}
+
+export default SecondBlog
